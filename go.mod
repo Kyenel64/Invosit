3ce335080e280de1168d17e41dffffbbfee2b66b
Kye Nelson <kyenelson3@gmail.com>
2026-05-13 08:15:04 +0900

    storage interface and s3 storage implementation

--- go.mod
+++ go.mod
@@ -4,12 +4,24 @@ go 1.26.2
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
+	github.com/aws/aws-sdk-go-v2 v1.41.7
+	github.com/aws/aws-sdk-go-v2/credentials v1.19.16
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.101.0
 	github.com/go-playground/validator/v10 v10.30.2
 	github.com/lib/pq v1.12.3
 	github.com/ory/client-go v1.22.39
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.7.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.9.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.23 // indirect
+	github.com/aws/smithy-go v1.25.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
---
+++ internal/storage/s3.go
@@ -0,0 +1,97 @@
+package storage
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+// s3Storage implements Storage against any S3-compatible object store
+// (AWS S3, Cloudflare R2, etc)
+type s3Storage struct {
+	client  *s3.Client
+	presign *s3.PresignClient
+	bucket  string
+}
+
+func newS3Storage(cfg Config) (*s3Storage, error) {
+	if cfg.Bucket == "" {
+		return nil, errors.New("storage: bucket is required")
+	}
+	if cfg.AccessKey == "" || cfg.SecretKey == "" {
+		return nil, errors.New("storage: access key and secret key are required")
+	}
+	if cfg.Region == "" {
+		return nil, errors.New("storage: region is required")
+	}
+
+	awsCfg := aws.Config{
+		Region:      cfg.Region,
+		Credentials: credentials.NewStaticCredentialsProvider(cfg.AccessKey, cfg.SecretKey, ""),
+	}
+
+	client := s3.NewFromConfig(awsCfg, func(o *s3.Options) {
+		if cfg.Endpoint != "" {
+			o.BaseEndpoint = aws.String(cfg.Endpoint)
+		}
+		o.UsePathStyle = cfg.UsePathStyle
+	})
+
+	return &s3Storage{
+		client:  client,
+		presign: s3.NewPresignClient(client),
+		bucket:  cfg.Bucket,
+	}, nil
+}
+
+func (s *s3Storage) SignedPutURL(ctx context.Context, key string, expiry time.Duration) (string, error) {
+	if err := validateExpiry(expiry); err != nil {
+		return "", err
+	}
+
+	req, err := s.presign.PresignPutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(key),
+	},
+		s3.WithPresignExpires(expiry),
+	)
+	if err != nil {
+		return "", fmt.Errorf("presign put %s: %w", key, err)
+	}
+
+	return req.URL, nil
+}
+
+func (s *s3Storage) SignedGetURL(ctx context.Context, key string, expiry time.Duration) (string, error) {
+	if err := validateExpiry(expiry); err != nil {
+		return "", err
+	}
+
+	req, err := s.presign.PresignGetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(key),
+	},
+		s3.WithPresignExpires(expiry),
+	)
+	if err != nil {
+		return "", fmt.Errorf("presign get %s: %w", key, err)
+	}
+
+	return req.URL, nil
+}
+
+func (s *s3Storage) Delete(ctx context.Context, key string) error {
+	_, err := s.presign.PresignDeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return fmt.Errorf("delete %s: %w", key, err)
+	}
+	return nil
+}
