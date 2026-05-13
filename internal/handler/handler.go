3966e158b591a75a1fbe8b3498ac5c739aeceae5
Kye Nelson <kyenelson3@gmail.com>
2026-05-13 09:19:47 +0900

    wire storage to main

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,14 +4,16 @@ import (
 	"database/sql"
 
 	"github.com/kyenel64/invosit-api/internal/kratos"
+	"github.com/kyenel64/invosit-api/internal/storage"
 )
 
 type Handler struct {
 	db         *sql.DB
 	kratos     *kratos.Client
+	blobs      storage.Storage
 	webhookKey string
 }
 
-func New(db *sql.DB, kc *kratos.Client, webhookKey string) *Handler {
-	return &Handler{db: db, kratos: kc, webhookKey: webhookKey}
+func New(db *sql.DB, kc *kratos.Client, blobs storage.Storage, webhookKey string) *Handler {
+	return &Handler{db: db, kratos: kc, blobs: blobs, webhookKey: webhookKey}
 }
